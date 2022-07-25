a1b79a9775f790f8ea8dc8816e79b145062a9686
bakiev75 <bakiev75@mail.ru>
2022-07-25 19:23:27 +0500

    add final version D1.6 exercise

--- finish_d1_6/finish_d1_6/urls.py
+++ finish_d1_6/finish_d1_6/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('page_1/', include('django.contrib.flatpages.urls')), # Будет установлен доступ по паролю администратора
     path('page_2/', include('django.contrib.flatpages.urls')), # Будут изменены шрифты и размер текста
     path('page_3/', include('django.contrib.flatpages.urls')), # контент дублируется без изменения content
+    path('page_home/', include('django.contrib.flatpages.urls'))
 ]
